2c3a77505aa18227f9638d6f6d9d7ad1720e2478
Matvey Esipov <matveybravo@gmail.com>
2025-05-09 16:17:07 +0300

    але

--- OKO-RTK/src/pages/authorization/AuthFooter.tsx
+++ OKO-RTK/src/pages/authorization/AuthFooter.tsx
@@ -1,20 +1,17 @@
 function Footer() {
 	return (
-		<footer className='overflow-hidden px-14 pt-6 pb-4 mt-20 w-full bg-gray-200 max-md:px-5 max-md:mt-10 max-md:max-w-full'>
+		<footer className='overflow-hidden px-14 pt-6 pb-4 w-full bg-[#E8E8EE] max-md:px-5 max-md:mt-10 max-md:max-w-full'>
 			<div className='flex flex-col w-full max-md:max-w-full'>
 				<div className='flex gap-2 justify-center items-end self-start'>
-					<span className='font text-base font-medium text-neutral-400'>
+					<span className='font text-[16px] font-medium text-[#9699A3]'>
 						Поддержка
 					</span>
-					<a
-						href='tel:88001000800'
-						className='text-lg font-[Inter] font-bold italic text-violet-700 hover:underline'
-					>
+					<p className='text-[18px] font-[Inter] font-bold text-[#7700FF]'>
 						8 800 100 0 800
-					</a>
+					</p>
 				</div>
 
-				<div className='flex flex-wrap gap-10 justify-between items-center mt-6 w-full text-base text-zinc-400 max-md:max-w-full'>
+				<div className='flex flex-wrap gap-10 justify-between items-center mt-6 w-full text-[16px] text-[#9699A3] max-md:max-w-full'>
 					<p className='self-stretch my-auto font-medium w-[652px] max-md:max-w-full'>
 						Продолжая пользование сайтом, вы соглашаетесь на обработку файлов{' '}
 						<a
