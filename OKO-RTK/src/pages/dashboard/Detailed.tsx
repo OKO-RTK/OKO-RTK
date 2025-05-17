08aeb1fee45df4a7d0844a655ff47c69c7d624fe
letyaga_gang <miheyka2005@gmail.com>
2025-05-17 15:55:26 +0300

    Границы

--- OKO-RTK/src/pages/dashboard/Detailed.tsx
+++ OKO-RTK/src/pages/dashboard/Detailed.tsx
@@ -10,33 +10,33 @@ import {
 function Detailed() {
   return (
 		<Box w='full'>
-			<Grid templateColumns='repeat(12, 1fr)' gap={4}>
+			<Grid templateColumns='repeat(12, 1fr)' gap={4} >
 				{/* Левая вертикальная колонка (1/12 ширины, растягивается) */}
-				<GridItem colSpan={2} rowSpan={2}>
+				<GridItem colSpan={2} rowSpan={2} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='100%' minH='150px' />
 				</GridItem>
 
 				{/* Верхний ряд, 2 блока */}
-				<GridItem colSpan={5}>
+				<GridItem colSpan={5} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='150px' />
 				</GridItem>
-				<GridItem colSpan={5}>
+				<GridItem colSpan={5} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='150px' />
 				</GridItem>
 
 				{/* Средний большой блок */}
-				<GridItem colSpan={10}>
+				<GridItem colSpan={10} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='200px' />
 				</GridItem>
 
 				{/* Три блока снизу */}
-				<GridItem colSpan={4}>
+				<GridItem colSpan={4} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='150px' />
 				</GridItem>
-				<GridItem colSpan={4}>
+				<GridItem colSpan={4} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='150px' />
 				</GridItem>
-				<GridItem colSpan={4}>
+				<GridItem colSpan={4} border={'1px solid #7700FF'}>
 					<Box bg='white' borderRadius='lg' h='150px' />
 				</GridItem>
 			</Grid>
