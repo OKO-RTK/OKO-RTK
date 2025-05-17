3d7a31fa6a1a9d45dfdc9e6550e6908d6b06c580
Matvey Esipov <matveybravo@gmail.com>
2025-05-17 16:25:35 +0300

    всратый general

--- OKO-RTK/src/pages/dashboard/General.tsx
+++ OKO-RTK/src/pages/dashboard/General.tsx
@@ -1,41 +1,106 @@
 import React, { useEffect, useState } from "react";
 import {
   Box,
-  Spinner,
-  Heading,
+  Grid,
+  GridItem,
   Flex,
   Select,
   Text
 } from "@chakra-ui/react";
 
 function General() {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<{ name: string } | null>(null);
-
-  useEffect(() => {
-    // Simulate data fetching
-    setTimeout(() => {
-      setData({ name: "General Data" });
-      setLoading(false);
-    });
-  }, []);
-
-  if (loading) {
-    return (
-      <Flex justifyContent="center" alignItems="center" height="100vh">
-        <Spinner size="xl" />
-      </Flex>
-    );
-  }
-
   return (
-    <Box p={4}>
-      <Heading as="h1" size="lg" mb={4}>
-        General Information
-      </Heading>
-      <Text fontSize="xl">{data?.name}</Text>
-    </Box>
-  );
+		<Box w='full'>
+			<Grid
+				templateColumns='233px 233px 487px 987px'
+				templateRows='repeat(4, 90px) 387px'
+				gap='20px'
+				bg='#F8F8F8'
+			>
+				{/* Верхние 4 маленьких блока слева */}
+				<GridItem colSpan={1} rowSpan={1}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+				<GridItem colStart={2} rowStart={1}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+				<GridItem colStart={1} rowStart={2}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+				<GridItem colStart={2} rowStart={2}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+
+				{/* Блок ниже четырех маленьких слева (ширина 487px, высота 320px) */}
+				<GridItem colStart={1} colSpan={2} rowStart={3} rowSpan={2}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+
+				{/* Правый большой блок (827x540) */}
+				<GridItem colStart={3} rowStart={1} rowSpan={4}>
+					<Box
+						w='100%'
+						h='100%'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+
+				{/* Нижний левый блок (333x387) */}
+				<GridItem colStart={1} colSpan={1} rowStart={5}>
+					<Box
+						w='333px'
+						h='387px'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+
+				{/* Нижний правый блок (987x387) */}
+				<GridItem colStart={3} colSpan={2} rowStart={5}>
+					<Box
+						w='987px'
+						h='387px'
+						bg='white'
+						border='1px solid #7700FF'
+						borderRadius='8px'
+					/>
+				</GridItem>
+			</Grid>
+		</Box>
+	)
 }
 
 export default General;
